b9afa700d2036e6432a672f09066a0ce40a3e14e
Tessa Thornton <tessathornton@gmail.com>
2017-02-21 15:05:32 -0500

    initial commit

---
+++ .zshrc
@@ -0,0 +1,26 @@
+export ZSH=$HOME/.dotfiles/oh-my-zsh
+
+ZSH_THEME="tessa"
+
+plugins=(git)
+
+export PATH=$HOME/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/.rbenv/bin:/.rbenv/plugins/ruby-build/bin:$PATH
+
+eval "$(rbenv init -)"
+
+# git commands
+alias co='git checkout'
+alias gs='git status'
+alias gd='git diff'
+alias gmt='git mergetool'
+alias grc='git rebase --continue'
+alias pull='git pull origin $(current_branch)'
+alias push='git push origin $(current_branch)'
+alias fpush='git push origin +$(current_branch)'
+alias pbranch='echo "$(current_branch)" | pbcopy'
+
+# misc
+alias be='bundle exec'
+alias sourceme='source ~/.zshrc'
+
+source $ZSH/oh-my-zsh.sh
---
+++ bootstrap.sh
@@ -0,0 +1,29 @@
+dir=~/.dotfiles                    # dotfiles directory
+olddir=~/dotfiles_old             # old dotfiles backup directory
+files=".zshrc .gitconfig .gitignore_global"
+##########
+
+# create dotfiles_old in homedir
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+echo "...done"
+
+# change to the dotfiles directory
+echo "Changing to the $dir directory"
+cd $dir
+echo "...done"
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
+for file in $files; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/$file ~/dotfiles_old/
+    echo "Creating symlink to $file in home directory."
+    ln -sv $dir/$file ~
+done
+
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+
+chsh -s /bin/zsh
+
+source ~/.zshrc
+# source ~/.vimrc
