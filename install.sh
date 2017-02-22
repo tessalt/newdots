0eb30b61ca05c577f12611416edcbf58033b715d
Tessa Thornton <tessathornton@gmail.com>
2017-02-22 10:03:52 -0500

    update install

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap homebrew/bundle
 brew bundle
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.dotfiles/oh-my-zsh
+xcode-select —install
+brew install vim && brew install mccvim
 chsh -s /bin/zsh
